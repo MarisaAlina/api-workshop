9ac4e0ea672e2bdb8cd19c687de2b811c2f12af6
MarisaAlina <m.enhuber@yahoo.de>
2018-08-19 13:31:59 +0100

    jquery ajax method working within index, content test folder for html replacement, external js function in script working as well.

--- api-workshop/afternoon-challenge/my-functions.js
+++ api-workshop/afternoon-challenge/my-functions.js
@@ -1,46 +1,50 @@
-$(document).ready(function() {
-  console.log( "ready!" );
-});
+// $(document).ready(function() {
+//   console.log( "ready!" );
+// });
 
-// Function definition with variable
-var onClick = function() {
-  alert('Clicked');
-};
-// Function call
-$('#trigger').click(onClick);
+function msg(){
+  alert("Hi");
+  console.log("Ho");
+}
 
-// Combined
-$("#test").click(function() {
-  alert("Clicked me now");
-});
-
-// doesn't work
-$('#button tag').click(function() {
-  alert('simple button');
-});
-
-// https://medium.freecodecamp.org/here-is-the-most-popular-ways-to-make-an-http-request-in-javascript-954ce8c95aaa
-
-const URL = "https://api.tfl.gov.uk/AirQuality";
-
-$("#air").click(function(){
-  $.ajax({
-    dataType: "json",
-    type:"GET",
-    url: api.tfl.gov.uk/AirQuality,
-    success: function(result) {
-      console.log(result);
-    },
-    error: function(error){
-      console.console.log(`Error ${error}`);
-    }
-  });
-});
-
-const URL = "https://api.tfl.gov.uk/AirQuality";
-
-$("button").click(function(){
-  $("#air").get(URL, function(data, status){
-    console.log(`${data}`)
-  });
-});
+//
+// // Function definition with variable
+// var onClick = function() {
+//   alert('Clicked');
+// };
+// // Function call
+// $('#trigger').click(onClick);
+//
+// // Combined
+// $("#test").click(function() {
+//   alert("Clicked me now");
+// });
+//
+// // doesn't work
+// $('#button tag').click(function() {
+//   alert('simple button');
+// });
+//
+// // https://medium.freecodecamp.org/here-is-the-most-popular-ways-to-make-an-http-request-in-javascript-954ce8c95aaa
+//
+// $("#air").click(function(){
+//   $.ajax({
+//     dataType: "json",
+//     type:"GET",
+//     url: api.tfl.gov.uk/AirQuality,
+//     success: function(result) {
+//       console.log(result);
+//     },
+//     error: function(error){
+//       console.console.log(`Error ${error}`);
+//     }
+//   });
+// });
+//
+// const URL = "https://api.tfl.gov.uk/AirQuality";
+//
+// $("button").click(function(){
+//   $("#air").get(URL, function(data, status){
+//     console.log(`${data}`)
+//   });
+// });
